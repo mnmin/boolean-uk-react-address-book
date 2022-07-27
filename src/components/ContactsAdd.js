309d686a47d3b4eafe6316f837ed07a0fd1a3a3a
Michele M <mnmin@gmail.com>
2022-07-27 12:10:19 +0100

    Contacts controlled form state properties handlers

--- src/components/ContactsAdd.js
+++ src/components/ContactsAdd.js
@@ -7,25 +7,39 @@ function ContactsAdd(props) {
   // to this component so new contacts can be added to the
   // state
   const { setContacts, contacts } = props
+  const [newContact, setNewContact] = useState({
+    firstName: "",
+    lastName: "",
+    street: "",
+    city: "",
+  })
 
+  console.log("contacts", contacts)
   //TODO: Implement controlled form
   //send POST to json server on form submit
+
+  function handleChange(event) {
+    const inputName = event.target.name
+    const inputValue = event.target.value
+    console.log("event", inputName, inputValue)
+    setNewContact({...newContact, [inputName]: inputValue})
+  }
 
   return (
     <form className="form-stack contact-form">
       <h2>Create Contact</h2>
 
       <label htmlFor="firstName">First Name</label>
-      <input id="firstName" name="firstName" type="text" required />
+      <input id="firstName" name="firstName" type="text" onChange={handleChange} required />
 
       <label htmlFor="lastName">Last Name:</label>
-      <input id="lastName" name="lastName" type="text" required/>
+      <input id="lastName" name="lastName" type="text" onChange={handleChange} required/>
 
       <label htmlFor="street">Street:</label>
-      <input id="street" name="street" type="text" required/>
+      <input id="street" name="street" type="text" onChange={handleChange} required/>
 
       <label htmlFor="city">City:</label>
-      <input id="city" name="city" type="text" required/>
+      <input id="city" name="city" type="text" onChange={handleChange} required/>
 
       <div className="actions-section">
         <button className="button blue" type="submit">
